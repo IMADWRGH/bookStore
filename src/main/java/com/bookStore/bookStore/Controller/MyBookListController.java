dedc32be8b23cf4d9ceffdcc6693ba041d988470
Imad Asserne <imad.asserne@gmail.com>
2023-11-15 21:18:27 +0100

    inject service and repository

--- src/main/java/com/bookStore/bookStore/Controller/MyBookListController.java
+++ src/main/java/com/bookStore/bookStore/Controller/MyBookListController.java
@@ -1,7 +1,14 @@
 package com.bookStore.bookStore.Controller;
 
+import com.bookStore.bookStore.Service.MyBookListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyBookListController {
+    private final MyBookListService myBookListService;
+    @Autowired
+    public MyBookListController(MyBookListService myBookListService) {
+        this.myBookListService = myBookListService;
+    }
 }
--- src/main/java/com/bookStore/bookStore/Service/MyBookListService.java
+++ src/main/java/com/bookStore/bookStore/Service/MyBookListService.java
@@ -1,7 +1,14 @@
 package com.bookStore.bookStore.Service;
 
+import com.bookStore.bookStore.Repository.MyBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyBookListService {
+    private final MyBookRepository myBookRepository;
+    @Autowired
+    public MyBookListService(MyBookRepository myBookRepository) {
+        this.myBookRepository = myBookRepository;
+    }
 }
