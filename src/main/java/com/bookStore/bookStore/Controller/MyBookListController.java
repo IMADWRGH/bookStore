33271a31b382f9b647a2c73c99e368de368f8b0a
Imad Asserne <imad.asserne@gmail.com>
2023-11-16 00:52:01 +0100

    add new endpoints

--- src/main/java/com/bookStore/bookStore/Controller/MyBookListController.java
+++ src/main/java/com/bookStore/bookStore/Controller/MyBookListController.java
@@ -1,8 +1,12 @@
 package com.bookStore.bookStore.Controller;
 
+
 import com.bookStore.bookStore.Service.MyBookListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class MyBookListController {
@@ -11,4 +15,10 @@ public class MyBookListController {
     public MyBookListController(MyBookListService myBookListService) {
         this.myBookListService = myBookListService;
     }
+    @RequestMapping(path = "/deleteMy/{id}")
+    public String deleteMyBook(@PathVariable("id") int id){
+        myBookListService.deleteBook(id);
+        return "redirect:/myBook";
+    }
+
 }
