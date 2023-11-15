7ddaf1bb288032df6597e23aa98bcef04b5a59d4
Imad Asserne <imad.asserne@gmail.com>
2023-11-16 00:49:51 +0100

    add 2 endpoints

--- src/main/java/com/bookStore/bookStore/Controller/BookController.java
+++ src/main/java/com/bookStore/bookStore/Controller/BookController.java
@@ -1,9 +1,12 @@
 package com.bookStore.bookStore.Controller;
 
 import com.bookStore.bookStore.Model.Book;
+import com.bookStore.bookStore.Model.MyBookList;
 import com.bookStore.bookStore.Service.BookService;
+import com.bookStore.bookStore.Service.MyBookListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,10 +16,12 @@ import java.util.List;
 @Controller
 public class BookController {
     private final BookService bookService;
+    private final MyBookListService myBookListService;
 
     @Autowired
-    public BookController(BookService bookService) {
+    public BookController(BookService bookService, MyBookListService myBookListService) {
         this.bookService = bookService;
+        this.myBookListService = myBookListService;
     }
 
     ////////// router navigate//////////////
@@ -30,10 +35,7 @@ public class BookController {
         return "bookRegister";
     }
 
-    @GetMapping(path = "/myBook")
-    public String myBook(){
-        return "myBook";
-    }
+
 
 
 
@@ -49,11 +51,30 @@ public class BookController {
         List<Book> list =bookService.getAllBooks();
         return  new ModelAndView("availableBook","book",list);
     }
-    @RequestMapping(path= "/myList/{id}")
-    public String getMyList(@PathVariable  int id){
+    @RequestMapping(path= "/mylist/{id}")
+    public String getMyList(@PathVariable("id")  int id){
        Book book= bookService.getBook(id);
-
+        MyBookList myBookList=new MyBookList(book.getId(),book.getName(),book.getAuthor(),book.getPrice());
+            myBookListService.addbbok(myBookList);
         return "redirect:/myBook";
+    }
+    @GetMapping(path = "/myBook")
+    public String myBook(Model model){
+        List<MyBookList> list=myBookListService.getList();
+        model.addAttribute("book",list);
+        return "myBook";
+    }
+
+    @RequestMapping(path = "/delete/{id}")
+    public String deleteBook(@PathVariable("id") int id){
+        bookService.deleteBook(id);
+        return "redirect:/availableBook";
+    }
+    @RequestMapping(path = "/edit/{id}")
+    public String editBook(@PathVariable("id") int id,Model model){
+         Book book=bookService.getBook(id);
+         model.addAttribute("book",book);
+        return "editBook";
     }
 
 
