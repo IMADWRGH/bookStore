89dfdee497a3f3c7b444e0a563ec71349b85f762
Imad Asserne <imad.asserne@gmail.com>
2023-11-16 00:53:22 +0100

    add function

--- src/main/java/com/bookStore/bookStore/Service/MyBookListService.java
+++ src/main/java/com/bookStore/bookStore/Service/MyBookListService.java
@@ -1,8 +1,11 @@
 package com.bookStore.bookStore.Service;
 
+import com.bookStore.bookStore.Model.MyBookList;
 import com.bookStore.bookStore.Repository.MyBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MyBookListService {
@@ -10,5 +13,16 @@ public class MyBookListService {
     @Autowired
     public MyBookListService(MyBookRepository myBookRepository) {
         this.myBookRepository = myBookRepository;
+    }
+    public void addbbok(MyBookList myBookList){
+        myBookRepository.save(myBookList);
+    }
+    public List<MyBookList> getList(){
+       return myBookRepository.findAll();
+
+    }
+
+    public void deleteBook(int id) {
+        myBookRepository.deleteById(id);
     }
 }
